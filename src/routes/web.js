8c114b53c775485f7fd8aa86bd08876f1f039bf4
Ian Kingori <ianm.kingori@gmail.com>
2019-02-22 02:52:54 +0300

    (chore) create web templates

--- src/routes/web.js
+++ src/routes/web.js
@@ -8,4 +8,12 @@ router.get('', (req, res) => {
   res.render('index');
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+router.get('/register', (req, res) => {
+  res.render('register');
+})
+
 module.exports = router;
